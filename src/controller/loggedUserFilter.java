52db4c9eaf80a6339a5c8371b2e9f80a43c5f5e9
FrancoMarcolongo <francomarcolongo98@gmail.com>
2023-11-03 20:46:47 -0300

    se agrega filtro para evitar ingresos sin login

---
+++ src/controller/loggedUserFilter.java
@@ -0,0 +1,45 @@
+package controller;
+
+import java.io.IOException;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Servlet Filter implementation class loggedUserFilter
+ */
+@WebFilter("/loggedUserFilter")
+public class loggedUserFilter implements Filter {
+	
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+		chain.doFilter(request, response);
+		/* 
+		 * DESCOMENTAR ESTA SECCIÓN PARA ACTIVAR FILTRO DE LOGIN Y BORRAR FILA 22
+		HttpServletRequest httpRequest = (HttpServletRequest) request;
+        HttpServletResponse httpResponse = (HttpServletResponse) response;
+        
+        // Verificar si el usuario está en la sesión
+        HttpSession session = httpRequest.getSession(false);
+        boolean isLoggedIn = session != null && session.getAttribute("loggedUser") != null;
+
+        // Si el usuario no está en la sesión y no está intentando acceder al login, redirigir al login
+        if (!isLoggedIn && !httpRequest.getRequestURI().endsWith("login.jsp") && !httpRequest.getRequestURI().endsWith("servletUsuario")) {
+            httpResponse.sendRedirect(httpRequest.getContextPath() + "/views/login.jsp");
+        } else {
+            chain.doFilter(request, response);
+        }
+        */
+	}
+
+	public void init(FilterConfig fConfig) throws ServletException {
+		// TODO Auto-generated method stub
+	}
+
+}
