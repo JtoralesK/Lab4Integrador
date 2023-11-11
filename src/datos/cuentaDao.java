200066a46ec0285892616d703da4e8f46b8d2216
javier torales <jtorales2016@gmail.com>
2023-11-11 00:22:57 -0300

    se refactorizo el mapeo del objeto cuenta

--- src/datos/cuentaDao.java
+++ src/datos/cuentaDao.java
@@ -27,17 +27,8 @@ public class cuentaDao {
 		try (PreparedStatement preparedStatement = connection.prepareStatement(query)) {
 			ResultSet rs = preparedStatement.executeQuery();
 			while (rs.next()) {
-				cuenta cta = new cuenta();
-				cta.setId_cuenta(Integer.parseInt(rs.getString("n_cuenta")));
-				cta.setId_cliente(Integer.parseInt(rs.getString("id_cliente")));
-				int tipoCuentaOrdinal = rs.getInt("id_tipo_cuenta") - 1;
-				cta.setId_tipo_cuenta(eTipoCuenta.values()[tipoCuentaOrdinal]);
-				cta.setSaldo(Double.valueOf(rs.getString("saldo")));
-				LocalDate fechaRegistro = rs.getDate("fecha_creacion").toLocalDate();
-				cta.setFecha_creacion(fechaRegistro);
-				cta.setCbu(rs.getString("cbu"));
+				cuenta cta = mapResultSetToCuenta(rs);
 				cuentas.add(cta);
-				cta.setEstado(rs.getBoolean("estado"));
 			}
 		} catch (SQLException e) {
 			e.printStackTrace();
@@ -46,79 +37,105 @@ public class cuentaDao {
 		}
 		return cuentas;
 	}
-	public List<cuenta> selectAllByTypeOf(eTipoCuenta e) {
+
+	public List<cuenta> selectAllByTypeOf(eTipoCuenta e, String prefix) {
 		// TODO Auto-generated method stub
-		List<cuenta> cuentas = new ArrayList<>();
-		Connection connection = cn.Open();
-		String query = "SELECT * FROM cuentas WHERE id_tipo_cuenta = ?";
+		List<cuenta> cuentas = null;
+		if (e != null || !prefix.isEmpty()) {
+			cuentas = new ArrayList<>();
+			Connection connection = cn.Open();
+			String query = "SELECT * FROM cuentas WHERE 1 = 1 ";
 
-	      try (PreparedStatement preparedStatement = connection.prepareStatement(query)) {
-	            preparedStatement.setInt(1,e==eTipoCuenta.CajaDeAhorro?1:2);
-	            ResultSet rs = preparedStatement.executeQuery();
-
-			while (rs.next()) {
-				cuenta cta = new cuenta();
-				cta.setId_cuenta(Integer.parseInt(rs.getString("n_cuenta")));
-				cta.setId_cliente(Integer.parseInt(rs.getString("id_cliente")));
-				int tipoCuentaOrdinal = rs.getInt("id_tipo_cuenta") - 1;
-				cta.setId_tipo_cuenta(eTipoCuenta.values()[tipoCuentaOrdinal]);
-				cta.setSaldo(Double.valueOf(rs.getString("saldo")));
-				LocalDate fechaRegistro = rs.getDate("fecha_creacion").toLocalDate();
-				cta.setFecha_creacion(fechaRegistro);
-				cta.setCbu(rs.getString("cbu"));
-				cuentas.add(cta);
-				cta.setEstado(rs.getBoolean("estado"));
+			if (e != null) {
+				query += " AND id_tipo_cuenta = ?";
 			}
-		} catch (SQLException err) {
-			err.printStackTrace();
+
+			if (!prefix.isEmpty()) {
+				query += " AND cbu LIKE ?";
+			}
+			try (PreparedStatement preparedStatement = connection.prepareStatement(query)) {
+				int paramIndex = 1;
+				if (e != null) {
+					preparedStatement.setInt(paramIndex++, e == eTipoCuenta.CajaDeAhorro ? 1 : 2);
+				}
+
+				if (!prefix.isEmpty()) {
+					preparedStatement.setString(paramIndex, prefix + "%");
+				}
+				ResultSet rs = preparedStatement.executeQuery();
+
+				while (rs.next()) {
+					cuenta cta = mapResultSetToCuenta(rs);
+					cuentas.add(cta);
+				}
+			} catch (SQLException err) {
+				err.printStackTrace();
+			} finally {
+				cn.close();
+			}
+		}
+
+		return cuentas;
+	}
+
+	public boolean insert(cuenta cta) {
+		boolean estado = true;
+		Connection connection = cn.Open();
+
+		String query = "INSERT INTO cuentas (n_cuenta, id_cliente, id_tipo_cuenta, saldo, fecha_creacion, cbu, estado) "
+				+ "VALUES (?, ?, ?, ?, ?, ?, ?)";
+
+		try (PreparedStatement preparedStatement = connection.prepareStatement(query)) {
+			preparedStatement.setInt(1, cta.getId_cuenta());
+			preparedStatement.setInt(2, cta.getId_cliente());
+			preparedStatement.setInt(3, cta.getId_tipo_cuenta().ordinal() + 1);
+			preparedStatement.setDouble(4, cta.getSaldo());
+			preparedStatement.setDate(5, java.sql.Date.valueOf(cta.getFecha_creacion()));
+			preparedStatement.setString(6, cta.getCbu());
+			preparedStatement.setBoolean(7, cta.isEstado());
+
+			estado = preparedStatement.executeUpdate() > 0;
+		} catch (SQLException e) {
+			e.printStackTrace();
+			estado = false;
 		} finally {
 			cn.close();
 		}
-		return cuentas;
-	}
-	public boolean insert(cuenta cta) {
-	    boolean estado = true;
-	    Connection connection = cn.Open();
 
-	    String query = "INSERT INTO cuentas (n_cuenta, id_cliente, id_tipo_cuenta, saldo, fecha_creacion, cbu, estado) " +
-	            "VALUES (?, ?, ?, ?, ?, ?, ?)";
-
-	    try (PreparedStatement preparedStatement = connection.prepareStatement(query)) {
-	        preparedStatement.setInt(1, cta.getId_cuenta());
-	        preparedStatement.setInt(2, cta.getId_cliente());
-	        preparedStatement.setInt(3, cta.getId_tipo_cuenta().ordinal() + 1);
-	        preparedStatement.setDouble(4, cta.getSaldo());
-	        preparedStatement.setDate(5, java.sql.Date.valueOf(cta.getFecha_creacion()));
-	        preparedStatement.setString(6, cta.getCbu());
-	        preparedStatement.setBoolean(7, cta.isEstado());
-
-	        estado = preparedStatement.executeUpdate() > 0;
-	    } catch (SQLException e) {
-	        e.printStackTrace();
-	        estado = false;
-	    } finally {
-	        cn.close();
-	    }
-
-	    return estado;
+		return estado;
 	}
 
 	public Boolean logicalDelete(int n_cuenta, int id_Cliente) {
-		 boolean estado = true;
+		boolean estado = true;
 
-		    try (Connection connection = cn.Open();
-		         PreparedStatement preparedStatement = connection.prepareStatement("UPDATE cuentas SET estado = 0 WHERE n_cuenta = ? AND id_cliente = ?")) {
+		try (Connection connection = cn.Open();
+				PreparedStatement preparedStatement = connection
+						.prepareStatement("UPDATE cuentas SET estado = 0 WHERE n_cuenta = ? AND id_cliente = ?")) {
 
-		        preparedStatement.setInt(1, n_cuenta);
-		        preparedStatement.setInt(2, id_Cliente);
+			preparedStatement.setInt(1, n_cuenta);
+			preparedStatement.setInt(2, id_Cliente);
 
-		        estado = preparedStatement.executeUpdate() > 0;
+			estado = preparedStatement.executeUpdate() > 0;
 
-		    } catch (SQLException e) {
-		        e.printStackTrace();
-		        estado = false; 
-		    }
+		} catch (SQLException e) {
+			e.printStackTrace();
+			estado = false;
+		}
 
-		    return estado;
+		return estado;
+	}
+
+	private cuenta mapResultSetToCuenta(ResultSet rs) throws SQLException {
+		cuenta cta = new cuenta();
+		cta.setId_cuenta(Integer.parseInt(rs.getString("n_cuenta")));
+		cta.setId_cliente(Integer.parseInt(rs.getString("id_cliente")));
+		int tipoCuentaOrdinal = rs.getInt("id_tipo_cuenta") - 1;
+		cta.setId_tipo_cuenta(eTipoCuenta.values()[tipoCuentaOrdinal]);
+		cta.setSaldo(Double.valueOf(rs.getString("saldo")));
+		LocalDate fechaRegistro = rs.getDate("fecha_creacion").toLocalDate();
+		cta.setFecha_creacion(fechaRegistro);
+		cta.setCbu(rs.getString("cbu"));
+		cta.setEstado(rs.getBoolean("estado"));
+		return cta;
 	}
 }
